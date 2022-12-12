6a6044f7a19a6f6ea751ed9a9d8d37f0d9ef7df3
per <perwaaler@gmail.com>
2022-12-12 16:00:28 +0100

    changed path generation; use only fullfile function to get path names

--- scripts/set up/paths.m
+++ scripts/set up/paths.m
@@ -5,18 +5,18 @@
 % data storage location respectively.
 
 % add path to the yaml utilities so that local yml file can be read:
-addpath(genpath('..\functions\yaml utilities'));
+addpath(genpath(fullfile('..', 'functions', 'yaml utilities'));
 % load local project and data paths from yml file (ignored by git):
 paths_local = yaml.loadFile('local_paths.yml');
 
-% Paths.project = paths_local.    
-Paths.functions = fullfile(paths_local.project, 'scripts\functions');  
+% Paths.project = paths_local.
+Paths.functions = fullfile(paths_local.project, 'script', 'functions');
 Paths.saveVar = fullfile(paths_local.project, 'saved matlab variables');
-Paths.setUpScripts = fullfile(paths_local.project, 'scripts\set up');
-Paths.murmurAlgorithm = fullfile(paths_local.project, 'scripts\T7 murmur algorithm');
+Paths.setUpScripts = fullfile(paths_local.project, 'scripts', 'set up');
+Paths.murmurAlgorithm = fullfile(paths_local.project, 'scripts', 'T7 murmur algorithm');
 Paths.dataFolder = paths_local.data;
 Paths.T7data = fullfile(paths_local.data, 'Data - excel og text filer');
-Paths.trainingAndTesting = fullfile(paths_local.project, 'scripts\model training and testing');
-Paths.figures = fullfile(paths_local.project, 'scripts\figures');
-Paths.tempSaveNets = fullfile(paths_local.project, 'saved matlab variables\trained networks\tempFiles');
-Paths.savedNets = fullfile(paths_local.project, 'saved matlab variables\trained networks');  
+Paths.trainingAndTesting = fullfile(paths_local.project, 'scripts', 'model training and testing');
+Paths.figures = fullfile(paths_local.project, 'scripts', 'figures');
+Paths.tempSaveNets = fullfile(paths_local.project, 'saved matlab variables', 'trained networks', 'tempFiles');
+Paths.savedNets = fullfile(paths_local.project, 'saved matlab variables', 'trained networks');
