ef42aa50b5829fcb55a58ba39de212e71ba856ea
per <perwaaler@gmail.com>
2022-12-12 15:07:05 +0100

    added a yaml file with local paths

--- scripts/set up/paths.m
+++ scripts/set up/paths.m
@@ -1,11 +1,22 @@
-% all folder paths which contain the relevant files for analysis:
-Paths.functions = 'C:\HS-project\scripts\functions';  
-Paths.saveVar = 'C:\HS-project\saved matlab variables';
-Paths.setUpScripts = 'C:\HS-project\scripts\set up';
-Paths.murmurAlgorithm = 'C:\HS-project\scripts\T7 murmur algorithm';
-Paths.dataFolder = 'C:\Users\perwa\OneDrive - UiT Office 365\Data - Lars Ailo Bongos files';
-Paths.T7data = 'C:\Users\perwa\OneDrive - UiT Office 365\Data - Lars Ailo Bongos files\Data - excel og text filer';
-Paths.trainingAndTesting = 'C:\HS-project\scripts\model training and testing';
-Paths.figures = 'C:\HS-project\scripts\figures';
-Paths.tempSaveNets = 'C:\HS-project\saved matlab variables\trained networks\tempFiles';
-Paths.savedNets = 'C:\HS-project\saved matlab variables\trained networks';  
+% all folder paths which contain the relevant files for analysis ** NOTE:
+% for this script to work, must be in the "set up" folder. NOTE: to run
+% this script, user must first create a yaml file with two fields,
+% "project" and "data", containing the paths to the project root folder and
+% data storage location respectively.
+
+% add path to the yaml utilities so that local yml file can be read:
+addpath(genpath('..\functions\yaml utilities'));
+% load local project and data paths from yml file (ignored by git):
+paths_local = yaml.loadFile('local_paths.yml');
+
+% Paths.project = paths_local.    
+Paths.functions = fullfile(paths_local.project, 'scripts\functions');  
+Paths.saveVar = fullfile(paths_local.project, 'saved matlab variables');
+Paths.setUpScripts = fullfile(paths_local.project, 'scripts\set up');
+Paths.murmurAlgorithm = fullfile(paths_local.project, 'scripts\T7 murmur algorithm');
+Paths.dataFolder = paths_local.data;
+Paths.T7data = fullfile(paths_local.data, 'Data - excel og text filer');
+Paths.trainingAndTesting = fullfile(paths_local.project, 'scripts\model training and testing');
+Paths.figures = fullfile(paths_local.project, 'scripts\figures');
+Paths.tempSaveNets = fullfile(paths_local.project, 'saved matlab variables\trained networks\tempFiles');
+Paths.savedNets = fullfile(paths_local.project, 'saved matlab variables\trained networks');  
